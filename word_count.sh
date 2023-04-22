61230da13ecee05803ff8e57b432ae84a07c2c6c
shady0x00 <shady.afifi.112@gmail.com>
2023-04-22 05:21:40 +0200

    refactor script

--- word_count.sh
+++ word_count.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Check if a directory path was provided as an argument
+if [ -z "$1" ]; then
+  echo "Please provide a directory path as an argument."
+  exit 1
+fi
+
+# Count the total number of words in all .txt files in the specified directory
 total_words=0
-for file in *.txt
+for file in "$1"/*.txt
 do
   words=$(cat "$file" | wc -w)
   total_words=$((total_words + words))
 done
 
-echo "Total number of words in *.txt files: $total_words"
+# Output the result
+echo "Total number of words in *.txt files in $1: $total_words"
 
